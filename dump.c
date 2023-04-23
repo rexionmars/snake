5fddec7c367f119f41195391ef5f8cb5483e976b
whoisleonardi <opensource.leonardi@gmail.com>
2023-04-23 17:04:57 -0300

    implement basic OPCODE DUMP

--- dump.c
+++ dump.c
@@ -19,18 +19,21 @@ void dump(uint64_t x)
      *   CAPACITY - index - 1 = 4
      */
     char buffer[32];
-    size_t buffer_size = 0;
+    size_t buffer_size = 1;
+    buffer[sizeof(buffer) - buffer_size ] = '\n';
 
-    while (x) {
+    do {
         buffer[sizeof(buffer) - buffer_size - 1] = (x % 10 + '0');
         buffer_size++;
         x /= 10;
-    }
+    } while (x);
+
     write(1, &buffer[sizeof(buffer) - buffer_size], buffer_size);
 }
 
 int main()
 {
     dump(69420);
+    dump(0);
     return 0;
 }
